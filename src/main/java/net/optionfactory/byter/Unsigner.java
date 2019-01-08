69de050294cf139a9c882962a777cbe5991a019c
Davide Salvador <davide@optionfactory.net>
2019-01-08 10:38:33 +0100

    :fix: using only jdk7 methods

--- src/main/java/net/optionfactory/byter/Unsigner.java
+++ src/main/java/net/optionfactory/byter/Unsigner.java
@@ -3,11 +3,15 @@ package net.optionfactory.byter;
 public class Unsigner {
 
     public static long BytetoUnsignedLong(int x) {
-        return ((long) x) & 255L;
+        return ((long) x) & 0xff;
+    }
+
+    public static int BytetoUnsignedInt(byte x) {
+        return ((int) x) & 0xff;
     }
 
     public static long IntegertoUnsignedLong(int x) {
-        return ((long) x) & 4294967295L;
+        return ((long) x) & 0xffffffffL;
     }
 
 }
